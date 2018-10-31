66ed6a27281e0120f4b71d590cd53ffe0026956c
Albert I <krascgq@outlook.co.id>
2018-10-31 10:44:54 +0800

    msm: mdss: Import X00TD changes
    * Extracted from S323 kernel source.
    * Remove ESD check disabling function (exclusive to HQ_BUILD_FACTORY)
      and we won't build the kernel with that flag.
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -4687,7 +4687,9 @@ static int mdss_fb_atomic_commit_ioctl(struct fb_info *info,
 			mfd->mdp.signal_retire_fence && mdp5_data)
 			mfd->mdp.signal_retire_fence(mfd,
 						mdp5_data->retire_cnt);
+#ifndef CONFIG_MACH_ASUS_X00TD
 		return 0;
+#endif
 	}
 
 	output_layer_user = commit.commit_v1.output_layer;
