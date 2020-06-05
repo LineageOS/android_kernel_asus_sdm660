11ad43acff144b46b6a98ba33227fe550e6ef167
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-06-05 23:18:26 +0530

    camera_v2: Import ASUS changes
    * from Q X01BD 424 kernel source
    
    Change-Id: I450c940699cfb6cb27310c6bc79b6d678855edec

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -33,7 +33,11 @@
 #define CYCLES_PER_MICRO_SEC_DEFAULT 4915
 #define CCI_MAX_DELAY 1000000
 
+#ifdef CONFIG_MACH_ASUS_SDM660
+#define CCI_TIMEOUT msecs_to_jiffies(1000)
+#else
 #define CCI_TIMEOUT msecs_to_jiffies(500)
+#endif
 
 /* TODO move this somewhere else */
 #define MSM_CCI_DRV_NAME "msm_cci"
@@ -1667,8 +1671,15 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 {
 	int32_t rc = 0;
 
+#ifdef CONFIG_MACH_ASUS_SDM660
+	struct cci_device *cci_dev;
+#endif
 	CDBG("%s line %d cmd %d\n", __func__, __LINE__,
 		cci_ctrl->cmd);
+#ifdef CONFIG_MACH_ASUS_SDM660
+	cci_dev = v4l2_get_subdevdata(sd);
+	mutex_lock(&cci_dev->mutex);
+#endif
 	switch (cci_ctrl->cmd) {
 	case MSM_CCI_INIT:
 		rc = msm_cci_init(sd, cci_ctrl);
@@ -1677,6 +1688,10 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 		rc = msm_cci_release(sd);
 		break;
 	case MSM_CCI_I2C_READ:
+#ifdef CONFIG_MACH_ASUS_SDM660
+		if (cci_dev->cci_state == CCI_STATE_DISABLED)
+			break;
+#endif
 		rc = msm_cci_i2c_read_bytes(sd, cci_ctrl);
 		break;
 	case MSM_CCI_I2C_WRITE:
@@ -1684,6 +1699,10 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 	case MSM_CCI_I2C_WRITE_SYNC:
 	case MSM_CCI_I2C_WRITE_ASYNC:
 	case MSM_CCI_I2C_WRITE_SYNC_BLOCK:
+#ifdef CONFIG_MACH_ASUS_SDM660
+		if (cci_dev->cci_state == CCI_STATE_DISABLED)
+			break;
+#endif
 		rc = msm_cci_write(sd, cci_ctrl);
 		break;
 	case MSM_CCI_GPIO_WRITE:
@@ -1697,6 +1716,9 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 	}
 	CDBG("%s line %d rc %d\n", __func__, __LINE__, rc);
 	cci_ctrl->status = rc;
+#ifdef CONFIG_MACH_ASUS_SDM660
+	mutex_unlock(&cci_dev->mutex);
+#endif
 	return rc;
 }
 
@@ -2093,6 +2115,9 @@ static int msm_cci_probe(struct platform_device *pdev)
 		pr_err("%s: no enough memory\n", __func__);
 		return -ENOMEM;
 	}
+#ifdef CONFIG_MACH_ASUS_SDM660
+	mutex_init(&new_cci_dev->mutex);
+#endif
 	v4l2_subdev_init(&new_cci_dev->msm_sd.sd, &msm_cci_subdev_ops);
 	snprintf(new_cci_dev->msm_sd.sd.name,
 			ARRAY_SIZE(new_cci_dev->msm_sd.sd.name), "msm_cci");
@@ -2190,6 +2215,9 @@ cci_invalid_vreg_data:
 cci_release_mem:
 	msm_camera_put_reg_base(pdev, new_cci_dev->base, "cci", true);
 cci_no_resource:
+#ifdef CONFIG_MACH_ASUS_SDM660
+	mutex_destroy(&new_cci_dev->mutex);
+#endif
 	kfree(new_cci_dev);
 	return rc;
 }
