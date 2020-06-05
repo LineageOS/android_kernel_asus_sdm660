ed713c24ac8b6daa127558910bc5630cbd2af826
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-06-05 13:27:42 +0530

    drivers: power: Import ASUS changes
    * from Q X01BD 424 kernel source
    
    Change-Id: I2d6494ae5a9c7d0da5b2064b55fbdc21b3847185

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -430,6 +430,9 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(pd_voltage_min),
 	POWER_SUPPLY_ATTR(sdp_current_max),
 	POWER_SUPPLY_ATTR(fg_reset_clock),
+#ifdef CONFIG_MACH_ASUS_SDM660
+	POWER_SUPPLY_ATTR(adapter_id),
+#endif
 	POWER_SUPPLY_ATTR(connector_type),
 	POWER_SUPPLY_ATTR(parallel_batfet_mode),
 	POWER_SUPPLY_ATTR(parallel_fcc_max),
