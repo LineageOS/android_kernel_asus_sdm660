99609a26d5075103c2867f2968cb09d6f9bf2181
Vivekachooz <vivekachooz@gmail.com>
2020-11-19 19:05:30 +0530

    drivers: input: X00TD: Import Semtech SX9310 SAR driver
    * From S424 kernel source
    
    Change-Id: I5e8e232e3d32e00e73ddd6299e38ec068fe98c4e

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -716,6 +716,11 @@
 #define KEY_TP_GESTURE_SWIPE_RIGHT	759
 #endif
 
+#ifdef CONFIG_MACH_ASUS_X00TD
+#define KEY_SARSENSOR_NEAR		0x2ea
+#define KEY_SARSENSOR_FAR		0x2eb
+#endif
+
 /* We avoid low common keys in module aliases so they don't get huge. */
 #define KEY_MIN_INTERESTING	KEY_MUTE
 #define KEY_MAX			0x2ff
@@ -771,6 +776,7 @@
 
 #define ABS_MISC		0x28
 
+#ifndef CONFIG_MACH_ASUS_X00TD
 /*
  * 0x2e is reserved and should not be used in input drivers.
  * It was used by HID as ABS_MISC+6 and userspace needs to detect if
@@ -779,6 +785,7 @@
  * the situation described above.
  */
 #define ABS_RESERVED		0x2e
+#endif
 
 #define ABS_MT_SLOT		0x2f	/* MT slot being modified */
 #define ABS_MT_TOUCH_MAJOR	0x30	/* Major axis of touching ellipse */
